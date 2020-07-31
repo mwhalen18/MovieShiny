73ef8196199fbe219f9be275fb20b5042b8e2273
mwhalen18 <matthew.whalen18@gmail.com>
2020-07-30 21:47:00 -0400

    Model Performance

--- Model Testing.R
+++ Model Testing.R
@@ -5,8 +5,24 @@ summary(ratingsmodel)
 set.seed(7106)
 
 newdata <- TMDb6000
-test <- predict(moneymodel, newdata)
+pred.revenue <- predict(moneymodel, newdata)
+pred.rating <- predict(ratingsmodel, newdata)
 
-poly <- cbind(TMDb6000, test)
+poly <- cbind(TMDb6000, pred.revenue)
+polyy <- cbind(poly, pred.rating)
 
-predictions <- lm(sqrt(revenue) ~ test, poly)
+predictions <- lm(sqrt(revenue) ~ pred.revenue, poly)
+predictions2 <- lm(vote_average ~ pred.rating, polyy)
+
+revenue.plot <- polyy %>% ggplot(aes(pred.revenue, sqrt(revenue))) + 
+  geom_point(alpha = 0.3) + 
+  geom_smooth(method = "lm", se = FALSE) + xlab("Model Predictions") + ylab("Actual Revenue")
+
+summary(predictions)$r.squared
+summary(predictions2)$r.squared
+
+rating.plot <- polyy %>% ggplot(aes(pred.rating, vote_average)) + 
+  geom_point(alpha = 0.3) + 
+  geom_smooth(method = "lm", se = FALSE) + xlab("Model Predictions") + ylab("Actual Revenue")
+
+
